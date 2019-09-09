2b8c4759b766d0f3ae924893e000ae3657d25148
mohak92 <tamhane.mohak@gmail.com>
2019-09-09 19:29:31 -0400

    added images created files implemented randon word selector

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var drivers = ["Sebastian Vettel", "Charles Leclerc", "Kimi Raikkonen", "Antonio", "Antonio Giovinazzi"
+                    , "Lewis Hamilton", "Valtteri Bottas", "Alexander Alborn", "Max Verstappen", "Michael Schumacher"];
+    var driverImages = [
+        "../assets/images/sebastianVettel.png", 
+        "", 
+        "", 
+        "", 
+        "", 
+        "", 
+        "", 
+        "", 
+        "../assets/images/max.jpg" 
+        ,""
+    ];
+    console.log(drivers.length)
+    console.log(driverImages.length)
+    var randomSelector = Math.floor(Math.random() * drivers.length);
+    console.log(drivers[randomSelector]);
+    console.log(randomSelector);
+    document.getElementById('image').src=driverImages[randomSelector];
+    document.onkeyup = function (event) {
+        
+    };
+});
+
+function reset() {
+    window.location.reload(true);
+}
