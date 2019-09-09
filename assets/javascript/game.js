31dcfdfac7b5195fe4718ad848d8180087d0b62b
mohak92 <tamhane.mohak@gmail.com>
2019-09-09 19:31:55 -0400

    bug fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var drivers = ["Sebastian Vettel", "Charles Leclerc", "Kimi Raikkonen", "Antonio", "Antonio Giovinazzi"
                     , "Lewis Hamilton", "Valtteri Bottas", "Alexander Alborn", "Max Verstappen", "Michael Schumacher"];
     var driverImages = [
-        "../assets/images/sebastianVettel.png", 
+        "Word-Guess-Game/assets/images/sebastianVettel.png", 
         "", 
         "", 
         "", 
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "", 
         "", 
         "", 
-        "../assets/images/max.jpg" 
+        "Word-Guess-Game/assets/images/max.jpg" 
         ,""
     ];
     console.log(drivers.length)
